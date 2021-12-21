5a79e3b86f208b5828d7d4a1ee5284462e7b8815
Chris Doehring <chrisdoehring@gmail.com>
2021-12-20 19:40:21 -0800

    Fix coding error on mission CA.

--- app/transform_service/smartconnect_transformers.py
+++ app/transform_service/smartconnect_transformers.py
@@ -79,7 +79,8 @@ class SmartEREventTransformer:
             self.ca = self.get_conservation_area(self.ca_uuid)
         except Exception as ex:
             self.logger.warning(f'Failed to get CA Metadata for endpoint: {config.endpoint}, username: {config.login}, CA-UUID: {self.ca_uuid}. Exception: {ex}.')
-
+            self.ca = None
+            
         # Let the timezone fall-back to configuration in the OutboundIntegration.
         try:
             val = self._config.additional.get('timezone', None)
