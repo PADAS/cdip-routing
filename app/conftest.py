09b08911af91d62d6fcd594a8bf316fba6248439
Mariano M <marianom@earthranger.com>
2023-02-07 16:02:33 -0300

    Add test coverage for functions using the gundi client

---
+++ app/conftest.py
@@ -0,0 +1,92 @@
+import pytest
+import asyncio
+
+
+def async_return(result):
+    f = asyncio.Future()
+    f.set_result(result)
+    return f
+
+
+@pytest.fixture
+def mock_cache(mocker):
+    mock_cache = mocker.MagicMock()
+    mock_cache.get.return_value = None
+    return mock_cache
+
+
+@pytest.fixture
+def mock_gundi_client(
+    mocker,
+    inbound_integration_config,
+    outbound_integration_config,
+    outbound_integration_config_list,
+):
+    mock_client = mocker.MagicMock()
+    mock_client.get_inbound_integration.return_value = async_return(
+        inbound_integration_config
+    )
+    mock_client.get_outbound_integration.return_value = async_return(
+        outbound_integration_config
+    )
+    mock_client.get_outbound_integration_list.return_value = async_return(
+        outbound_integration_config_list
+    )
+    return mock_client
+
+
+@pytest.fixture
+def inbound_integration_config():
+    return {
+        "state": {},
+        "id": "12345b4f-88cd-49c4-a723-0ddff1f580c4",
+        "type": "1234e5bd-a473-4c02-9227-27b6134615a4",
+        "owner": "1234191a-bcf3-471b-9e7d-6ba8bc71be9e",
+        "endpoint": "https://logins.testbidtrack.co.za/restintegration/",
+        "login": "test",
+        "password": "test",
+        "token": "",
+        "type_slug": "bidtrack",
+        "provider": "bidtrack",
+        "default_devicegroup": "1234cfdc-1aae-44b0-8e0a-22c72355ea85",
+        "enabled": True,
+        "name": "BidTrack - Manyoni",
+    }
+
+
+@pytest.fixture
+def outbound_integration_config():
+    return {
+        "id": "2222dc7e-73e2-4af3-93f5-a1cb322e5add",
+        "type": "f61b0c60-c863-44d7-adc6-d9b49b389e69",
+        "owner": "1111191a-bcf3-471b-9e7d-6ba8bc71be9e",
+        "name": "[Internal] AI2 Test -  Bidtrack to  ER",
+        "endpoint": "https://cdip-er.pamdas.org/api/v1.0",
+        "state": {},
+        "login": "",
+        "password": "",
+        "token": "1111d87681cd1d01ad07c2d0f57d15d6079ae7d7",
+        "type_slug": "earth_ranger",
+        "inbound_type_slug": "bidtrack",
+        "additional": {},
+    }
+
+
+@pytest.fixture
+def outbound_integration_config_list():
+    return [
+        {
+            "id": "2222dc7e-73e2-4af3-93f5-a1cb322e5add",
+            "type": "f61b0c60-c863-44d7-adc6-d9b49b389e69",
+            "owner": "1111191a-bcf3-471b-9e7d-6ba8bc71be9e",
+            "name": "[Internal] AI2 Test -  Bidtrack to  ER",
+            "endpoint": "https://cdip-er.pamdas.org/api/v1.0",
+            "state": {},
+            "login": "",
+            "password": "",
+            "token": "1111d87681cd1d01ad07c2d0f57d15d6079ae7d7",
+            "type_slug": "earth_ranger",
+            "inbound_type_slug": "bidtrack",
+            "additional": {},
+        }
+    ]
---
+++ app/transform_service/tests/test_get_destinations.py
@@ -0,0 +1,19 @@
+import pytest
+from app.transform_service.services import get_all_outbound_configs_for_id
+from gundi_client import schemas
+
+
+@pytest.mark.asyncio
+async def test_get_all_outbound_configs_for_id(
+    mocker, mock_cache, mock_gundi_client, outbound_integration_config_list
+):
+    # Mock external dependencies
+    mocker.patch("app.transform_service.services._cache_db", mock_cache)
+    mocker.patch("app.transform_service.services.portal", mock_gundi_client)
+    destinations = await get_all_outbound_configs_for_id(
+        inbound_id="12345b4f-88cd-49c4-a723-0ddff1f580c4", device_id="12345"
+    )
+    assert destinations == [
+        schemas.OutboundConfiguration.parse_obj(conf)
+        for conf in outbound_integration_config_list
+    ]
