7bda59b402af5101834bd18d06c76f94c61f51c4
Mariano M <marianom@earthranger.com>
2023-03-30 10:26:08 -0300

    Add test coverage for retries

--- app/subscribers/tests/test_process_observations.py
+++ app/subscribers/tests/test_process_observations.py
@@ -81,3 +81,71 @@ async def test_process_observation_and_route_to_kafka_dispatcher_by_default(
     await process_observation(None, unprocessed_observation_position)
     # Check that the right method, to publish to kafka, was called
     assert mock_kafka_topic.send.called
+
+
+@pytest.mark.asyncio
+async def test_retry_observations_sent_to_gcp_pubsub_on_timeout(
+    mocker,
+    mock_cache,
+    mock_gundi_client,
+    mock_pubsub_client_with_timeout_once,
+    mock_kafka_topic,
+    unprocessed_observation_position,
+    outbound_configuration_gcp_pubsub,
+):
+    # Mock external dependencies
+    mock_gundi_client.get_outbound_integration_list.return_value = async_return(
+        [outbound_configuration_gcp_pubsub]
+    )
+    mocker.patch("app.transform_service.services._cache_db", mock_cache)
+    mocker.patch("app.transform_service.services._portal", mock_gundi_client)
+    # The mocked PubSub client raises an asyncio.TimeoutError in the first call, and returns success in a second call
+    mocker.patch("app.subscribers.kafka_subscriber.pubsub", mock_pubsub_client_with_timeout_once)
+    mocker.patch(
+        "app.subscribers.kafka_subscriber.observations_transformed_topic",
+        mock_kafka_topic,
+    )
+    mocker.patch(
+        "app.subscribers.kafka_subscriber.observations_unprocessed_deadletter",
+        mock_kafka_topic,
+    )
+    await process_observation(None, unprocessed_observation_position)
+    # Check that the right methods, to publish to PuSub, were called
+    assert mock_pubsub_client_with_timeout_once.PublisherClient.called
+    # The Publish method should be called twice due to the retry
+    assert mock_pubsub_client_with_timeout_once.PublisherClient.return_value.publish.called
+    assert mock_pubsub_client_with_timeout_once.PublisherClient.return_value.publish.call_count == 2
+
+
+@pytest.mark.asyncio
+async def test_retry_observations_sent_to_gcp_pubsub_on_client_error(
+    mocker,
+    mock_cache,
+    mock_gundi_client,
+    mock_pubsub_client_with_client_error_once,
+    mock_kafka_topic,
+    unprocessed_observation_position,
+    outbound_configuration_gcp_pubsub,
+):
+    # Mock external dependencies
+    mock_gundi_client.get_outbound_integration_list.return_value = async_return(
+        [outbound_configuration_gcp_pubsub]
+    )
+    mocker.patch("app.transform_service.services._cache_db", mock_cache)
+    mocker.patch("app.transform_service.services._portal", mock_gundi_client)
+    # The mocked PubSub client raises an asyncio.TimeoutError in the first call, and returns success in a second call
+    mocker.patch("app.subscribers.kafka_subscriber.pubsub", mock_pubsub_client_with_client_error_once)
+    mocker.patch(
+        "app.subscribers.kafka_subscriber.observations_transformed_topic",
+        mock_kafka_topic,
+    )
+    mocker.patch(
+        "app.subscribers.kafka_subscriber.observations_unprocessed_deadletter",
+        mock_kafka_topic,
+    )
+    await process_observation(None, unprocessed_observation_position)
+    # Check that the right methods, to publish to PuSub, were called
+    assert mock_pubsub_client_with_client_error_once.PublisherClient.called
+    # The Publish method should be called twice due to the retry
+    assert mock_pubsub_client_with_client_error_once.PublisherClient.return_value.publish.called
+    assert mock_pubsub_client_with_client_error_once.PublisherClient.return_value.publish.call_count == 2
