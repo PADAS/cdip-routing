06bcc14aebea83729791e54df8b978107140817b
Victor H. García <victorhugogarcia13d@gmail.com>
2023-08-08 13:49:31 -0600

    SIK-2263: Add more values validations in test

--- app/transform_service/tests/test_transform_observations_v1.py
+++ app/transform_service/tests/test_transform_observations_v1.py
@@ -1,4 +1,7 @@
 import pytest
+import pytz
+from datetime import datetime
+
 from app.transform_service.services import transform_observation
 from app.transform_service import helpers
 from app.subscribers.services import extract_fields_from_message, convert_observation_to_cdip_schema
@@ -21,7 +24,10 @@ def test_movebank_transformer(
     expected_keys = ["recorded_at", "tag_id", "lon", "lat", "sensor_type", "tag_manufacturer_id", "gundi_urn"]
     for key in transformed_observation.keys():
         assert key in expected_keys
-
+    # Check transformed_observation values
+    date_format = '%Y-%m-%dT%H:%M:%SZ'
+    assert bool(datetime.strptime(transformed_observation["recorded_at"], date_format)) is True
+    assert transformed_observation["recorded_at"] == observation.recorded_at.astimezone(tz=pytz.utc).strftime('%Y-%m-%dT%H:%M:%SZ')
     assert transformed_observation["tag_id"] == \
            f'{outbound_configuration_default.inbound_type_slug}.{observation.device_id}.{observation.integration_id}'
     assert transformed_observation["gundi_urn"] == \
